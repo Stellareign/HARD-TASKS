79ffd03c0d6d8d175c5a81937e2df6e202789784
Stellareign <tuguran@mail.ru>
2022-11-15 22:06:58 +0300

    Условные операторы

--- src/Main.java
+++ src/Main.java
@@ -10,36 +10,31 @@ public class Main {
         int salary = scanner.nextInt();
         int elevatedCreditAge = 23;
         double credit = 0;
-       int minAgeCoefficient = 2;
-       int maxAgeCoefficient = 3;
-       int minSalaryRub = 50_000;
-       int middleSalaryRub = 80_000;
-       double minSalaryCoefficient = 1.2;
-       double maxSalaryCoefficient = 1.3;
-        if (age < 18) {
-            System.out.println("Вам нет 18-ти лет, мы не можем выдать вам кредит.");
+        int minAgeCoefficient = 2;
+        int maxAgeCoefficient = 3;
+        int minSalaryRub = 50_000;
+        int middleSalaryRub = 80_000;
+        double minSalaryCoefficient = 1.2;
+        double maxSalaryCoefficient = 1.3;
+        if (age < 18 || age > 70) {
+            System.out.println("Мы не можем выдать вам кредит.");
         }
-        if (age > 18 && age < elevatedCreditAge && salary < minSalaryRub) {
-            credit = salary * minAgeCoefficient;
-        }
-        else if (age > elevatedCreditAge && salary < minSalaryRub) {
-            credit = salary * maxAgeCoefficient;
-        }
-        else if (age > 18 && age < elevatedCreditAge && salary > minSalaryRub && salary < middleSalaryRub) {
-            credit = salary * minAgeCoefficient * minSalaryCoefficient;
-        }
-        else if (age > elevatedCreditAge && salary > minSalaryRub && salary < middleSalaryRub) {
-            credit = salary * maxAgeCoefficient * minSalaryCoefficient;
-        }
-        else if (age > 18 && age < elevatedCreditAge && salary > middleSalaryRub) {
-            credit = salary * minAgeCoefficient * maxSalaryCoefficient;
-        }
-        else if (age > elevatedCreditAge && salary > middleSalaryRub) {
-            credit = salary * maxAgeCoefficient * maxSalaryCoefficient;
-        }
-        System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + credit +" рублей.");
-        }
-
-        }
+            else if (age < elevatedCreditAge && salary < minSalaryRub) {
+                credit = salary * minAgeCoefficient;
+            } else if (age > elevatedCreditAge && salary < minSalaryRub) {
+                credit = salary * maxAgeCoefficient;
+            } else if (age < elevatedCreditAge && salary > minSalaryRub && salary < middleSalaryRub) {
+                credit = salary * minAgeCoefficient * minSalaryCoefficient;
+            } else if (age > elevatedCreditAge && salary > minSalaryRub && salary < middleSalaryRub) {
+                credit = salary * maxAgeCoefficient * minSalaryCoefficient;
+            } else if (age < elevatedCreditAge && salary > middleSalaryRub) {
+                credit = salary * minAgeCoefficient * maxSalaryCoefficient;
+            } else if (age > elevatedCreditAge && salary > middleSalaryRub) {
+                credit = salary * maxAgeCoefficient * maxSalaryCoefficient;
+            }
+        System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + credit + " рублей.");
+        main(args);
+    }
+}
 
 
